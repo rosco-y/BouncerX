62220a17432607390c0d909fe0016b50911dfaba
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-02-21 17:06:57 -0500

    Now using a IValueConverter to add the word "Attendants" to the AttendantCount value when Binding it to a TextBlock, as we're doing in this sample.  i.e., "0 Attendants"

---
+++ BouncerX/OccasionConverter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BouncerX
+{
+    class OccasionConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert qa databound object into a value to be displayed.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public object Convert(object value,
+            object parameter,
+            string language)
+        {
+            if (value is Occasion)
+            {
+                var occastion = value as Occasion;
+                var ui_command = parameter as string;
+                if (ui_command == "number of attendants")
+                    return $"{occastion.AttendanceCount} Attendants";
+            }
+            return "Uknown number of Attendants";
+        }
+
+        
+        /// <summary>
+        /// Convert from bound value to displayable value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public object Convert(object value, 
+            Type targetType, 
+            object parameter, 
+            string language)
+        {
+            if (value is Occasion)
+            {
+                var occasion = value as Occasion;
+                var ui_command = parameter as string;
+                if (ui_command == "number of attendants")
+                    return $"{occasion.AttendanceCount} Attendants";
+            }
+            return "Uknown number of attendants";
+        }
+        /// <summary>
+        /// Saving Binding data back to databound object.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
+    
